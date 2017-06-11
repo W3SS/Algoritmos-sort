82a689887e001cbde77dd95f61136137639c0b7c
marcelojava <marcelo.iron09@gmail.com>
2017-06-11 13:02:12 -0300

    reduzindo codigo

--- src/insertionSort/InsertionSort.java
+++ src/insertionSort/InsertionSort.java
@@ -2,20 +2,15 @@ package insertionSort;
 
 public class InsertionSort {
 
-	public int[] insertSort(int[] array) {
+	public int[] sort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
-			boolean hasChange = true;
 			int countSubArranjo = i;
-
-			while (hasChange && countSubArranjo > 0) {
-				if (array[countSubArranjo] < array[countSubArranjo - 1]) {
-					final int aux = array[countSubArranjo];
-					array[countSubArranjo] = array[countSubArranjo - 1];
-					array[countSubArranjo - 1] = aux;
-					countSubArranjo--;
-				} else {
-					hasChange = false;
-				}
+			
+			while (countSubArranjo > 0 && array[countSubArranjo] < array[countSubArranjo - 1]) {
+				final int aux = array[countSubArranjo];
+				array[countSubArranjo] = array[countSubArranjo - 1];
+				array[countSubArranjo - 1] = aux;
+				countSubArranjo--;
 			}
 		}
 
--- src/insertionSort/InsertionSortMain.java
+++ src/insertionSort/InsertionSortMain.java
@@ -5,7 +5,7 @@ public class InsertionSortMain {
 	public static void main(String[] args) {
 		InsertionSort insertSort = new InsertionSort();
 		int[] array = {0,6,89,1,34, 23, 0};
-		array = insertSort.insertSort(array);
+		array = insertSort.sort(array);
 		
 		for (int i : array) {
 			System.out.println(i);
